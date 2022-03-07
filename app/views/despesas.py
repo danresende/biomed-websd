102b612bcc7005aacc65651a721dabbfaa5ef3ee
Daniel Resende <resende.d@gmail.com>
2022-03-07 11:13:39 -0300

    [teste] dias úteis

--- app/views/despesas.py
+++ app/views/despesas.py
@@ -23,7 +23,7 @@ def teste_politica_pgto(despesa):
     delta = delta.days + 1
     wd_delta = busday_count(hoje.strftime('%Y-%m-%d'), data_pgto.strftime('%Y-%m-%d'))
     
-    mensagem_teste = 'Número de dias úteis:' + wd_delta
+    mensagem_teste = 'Número de dias úteis:' + str(wd_delta)
     print(mensagem_teste)
     
     mensagem = 'Este pagamento está fora da política de pagamentos. Por favor, verifique se o motivo da urgência está descrito.'
